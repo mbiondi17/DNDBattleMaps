b1cda2d1d81d7efa4619e6d8d2655aff739b9bb5
Matt Biondi <mvb5bg@virginia.edu>
2018-08-06 18:15:29 -0400

    Start for Networking

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	private List<Unit> playersUnits;
+
+	// Use this for initialization
+	void Start () {
+		if(GetComponentInParent<UnityEngine.Networking.NetworkIdentity>().isServer) {
+			Debug.Log("I am a server!");
+			GameObject[] allUnits = GameObject.FindGameObjectsWithTag("Selectable");
+			foreach(GameObject unit in allUnits) {
+				if(unit.GetComponent<Unit>() != null)
+					this.playersUnits.Add(unit.GetComponent<Unit>());
+					unit.GetComponent<Unit>().SetControllingPlayer(this);
+			}
+		}
+		else {
+			Debug.Log("I am a client!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddUnit(GameObject newUnit) {
+		if(newUnit.GetComponent<Unit>() != null) {
+			this.playersUnits.Add(newUnit.GetComponent<Unit>());
+		}
+	}
+
+	public void RemoveUnit( GameObject unitToRemove) {
+		if(unitToRemove.GetComponent<Unit>() != null)
+			this.playersUnits.Remove(unitToRemove.GetComponent<Unit>());
+	}
+
+	public void transferUnit(GameObject unit, GameObject otherPlayer) {
+		if(otherPlayer.GetComponent<PlayerControl>() != null) {
+			otherPlayer.GetComponent<PlayerControl>().AddUnit(unit);
+			RemoveUnit(unit);
+		}
+	}
+}
--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Unit : MonoBehaviour {
 
 	private bool selected;
+	private PlayerControl controllingPlayer;
 
 	// Use this for initialization
 	void Start () {
@@ -13,25 +14,32 @@ public class Unit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0)) {
-			Ray tryRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit thisHit;
-			if(selected) {
-				if (Physics.Raycast (tryRay, out thisHit, 5000)) {
-					float terrainHeightAtHit = Terrain.activeTerrain.SampleHeight(thisHit.point);
-					float gridSnapX = (int)thisHit.point.x % 2 == 0 ? Mathf.Ceil(thisHit.point.x) : Mathf.Floor(thisHit.point.x);
-					float gridSnapZ = (int)thisHit.point.z % 2 == 0 ? Mathf.Ceil(thisHit.point.z) : Mathf.Floor(thisHit.point.z);
-					this.GetComponent<Transform>().position = new Vector3(gridSnapX, terrainHeightAtHit, gridSnapZ);
+		if(controllingPlayer != null) {
+			Debug.Log("Had contorller");
+			if(Input.GetMouseButtonDown(0)) {
+				Ray tryRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit thisHit;
+				if(selected) {
+					if (Physics.Raycast (tryRay, out thisHit, 5000)) {
+						float terrainHeightAtHit = Terrain.activeTerrain.SampleHeight(thisHit.point);
+						float gridSnapX = (int)thisHit.point.x % 2 == 0 ? Mathf.Ceil(thisHit.point.x) : Mathf.Floor(thisHit.point.x);
+						float gridSnapZ = (int)thisHit.point.z % 2 == 0 ? Mathf.Ceil(thisHit.point.z) : Mathf.Floor(thisHit.point.z);
+						this.GetComponent<Transform>().position = new Vector3(gridSnapX, terrainHeightAtHit, gridSnapZ);
+					}
+					this.selected = false;
 				}
-				this.selected = false;
-			}
-			else {
-				if (Physics.Raycast (tryRay, out thisHit, 5000)) {
-					if(thisHit.transform.gameObject == this.gameObject) {
-						this.selected = true;
+				else {
+					if (Physics.Raycast (tryRay, out thisHit, 5000)) {
+						if(thisHit.transform.gameObject == this.gameObject) {
+							this.selected = true;
+						}
 					}
 				}
 			}
 		}
+	}
+
+	public void SetControllingPlayer(PlayerControl player) {
+		this.controllingPlayer = player;
 	}
 }
