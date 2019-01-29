1812132a287532ef1b5fc67f0ba37a27127a046a
mbiondi17 <mvb5bg@virginia.edu>
2019-01-28 23:40:30 -0500

    POV selection, removable objects, and some new models / level work

---
+++ Assets/Scripts/CameraBounds.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour
+{
+
+    public GameObject boundingBox;
+    
+    private float xMin;
+    private float yMin;
+    private float zMin;
+    private float xMax;
+    private float yMax;
+    private float zMax;
+    
+    
+
+
+    void Start()
+    {
+        var rend = boundingBox.GetComponent<Renderer>();
+
+        xMin = rend.bounds.min.x;
+        yMin = rend.bounds.min.y;
+        zMin = rend.bounds.min.z;
+
+        xMax = rend.bounds.max.x;
+        yMax = rend.bounds.max.y;
+        zMax = rend.bounds.max.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var clampedX = Mathf.Clamp(transform.position.x, xMin, xMax);
+        var clampedY = Mathf.Clamp(transform.position.y, yMin, yMax);
+        var clampedZ = Mathf.Clamp(transform.position.z, zMin, zMax);
+        
+        this.transform.position = new Vector3(clampedX, clampedY, clampedZ);
+    }
+}
---
+++ Assets/Scripts/Removable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Removable : MonoBehaviour
+{
+    private bool visible = true;
+
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.V) && Input.GetMouseButtonDown(0)) {
+			Ray tryRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit thisHit;
+			if (Physics.Raycast (tryRay, out thisHit, 5000)) {
+				if(thisHit.transform.gameObject == this.gameObject) {
+                    Renderer rend;
+                    if(this.GetComponent<Renderer>() != null) 
+                            rend = this.GetComponent<Renderer>();
+                    else rend = this.GetComponentInChildren<Renderer>();
+                    
+                    if(visible) {
+                        rend.enabled = false;
+                        visible = false;
+                    }
+                    else {
+                        rend.enabled = true;
+                        visible = true;
+                    }
+                }
+			}
+		}
+    }
+}
